4b016e0d39ac96b29af5c0fa4ee1f6554bf7d976
Josiney Junior <josineyjunior14@gmail.com>
2021-01-15 12:13:05 -0300

    aula 04

--- ConverterTemperatura/src/app/home/home.page.ts
+++ ConverterTemperatura/src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    escalaDe: string;
+    temperaturaDe: number;
+    escalaPara: string;
+    temperaturaPara: number;
 
-  constructor() {}
+    constructor() {
+    }
 
+    converterParaCelcius(tempertura: number): number {
+        return (5 * (tempertura - 32) / 9);
+    }
+
+    converterParaFahrenheit(temperatura: number): number {
+        return (temperatura / 5 * 9 + 32);
+    }
+
+    async converter(): Promise<void> {
+        if (this.escalaPara === 'c') {
+            this.temperaturaPara = this.converterParaCelcius(this.temperaturaDe);
+        } else {
+            if (this.escalaPara === 'f') {
+                this.temperaturaPara = this.converterParaFahrenheit(this.temperaturaDe);
+            }
+        }
+    }
+
+    async limpar(): Promise<void> {
+        this.escalaPara = null;
+        this.escalaDe = null;
+        this.escalaPara = null;
+        this.temperaturaDe = null;
+    }
 }
